1784a2f51a05a0d2287f47bdb297c8bcf29e701e
Diego De Nova <diego.dnova@gmail.com>
2014-03-13 19:08:16 +0000

    perl file has changed

--- manifests/perl.pp
+++ manifests/perl.pp
@@ -7,8 +7,8 @@
 		$perl 		= 'perl',
 		$pmtools 	= 'pmtools',
 		$cpanminus 	= 'cpanminus',
-		$module		= ['CGI', 'Date::Format', 'DateTime'],
-						#'DAteTime::TimeZone',
+		#$module		= ['CGI', 'Date::Format', 'DateTime'],
+						#'DateTime::TimeZone',
 						#'DBI',
 						#'DBD::mysql',
 						#'Digest::SHA',
@@ -22,6 +22,7 @@
 		$path 		= '/usr/bin:/usr/sbin:/bin'
 		#Vagrant users $path 	= '/bin:/usr/bin'
 	){
+		#Installation of perl, pmtools and cpanminus
 		package { $perl:
 			ensure 	=> $ensure,
 		}
@@ -35,9 +36,88 @@
 			command 	=> "cpanm {$module[0]}",
   			path 		=> $path,
   			user 		=> root,
- 			#onlyif  	=> "test `puppet module list | grep puppetlabs-mysql | wc -l` -eq 0"
+ 			#onlyif  	=> "test `puppet module list | grep {$module[0]} | wc -l` -eq 0"
+		}	
+		#Installation of perl modules	
+		exec { 'CGI':
+			command 	=> "cpanm CGI",
+  			path 		=> $path,
+  			user 		=> root,
+ 			onlyif  	=> "test `puppet module list | grep CGI | wc -l` -eq 0"
 		}		
+		exec { 'Date-Format':
+			command 	=> "cpanm Date::Format'",
+  			path 		=> $path,
+  			user 		=> root,
+ 			onlyif  	=> "test `puppet module list | grep Date::Format' | wc -l` -eq 0"
+		}	
+		exec { 'DateTime':
+			command 	=> "cpanm DateTime",
+  			path 		=> $path,
+  			user 		=> root,
+ 			onlyif  	=> "test `puppet module list | grep DateTime | wc -l` -eq 0"
+		}			
+		exec { 'DateTime-TimeZone':
+			command 	=> "cpanm DateTime::TimeZone",
+  			path 		=> $path,
+  			user 		=> root,
+ 			onlyif  	=> "test `puppet module list | grep DateTime::TimeZone | wc -l` -eq 0"
+		}	
+		exec { 'DBI':
+			command 	=> "cpanm DBI",
+  			path 		=> $path,
+  			user 		=> root,
+ 			onlyif  	=> "test `puppet module list | grep DBI | wc -l` -eq 0"
+		}
+		exec { 'DBD-mysql':
+			command 	=> "cpanm DBD::mysql",
+  			path 		=> $path,
+  			user 		=> root,
+ 			onlyif  	=> "test `puppet module list | grep DBD::mysql | wc -l` -eq 0"
+		}		
+		exec { 'Digest-SHA':
+			command 	=> "cpanm Digest::SHA",
+  			path 		=> $path,
+  			user 		=> root,
+ 			onlyif  	=> "test `puppet module list | grep Digest::SHA | wc -l` -eq 0"
+		}
+		exec { 'Email-Send':
+			command 	=> "cpanm Email::Send",
+  			path 		=> $path,
+  			user 		=> root,
+ 			onlyif  	=> "test `puppet module list | grep Email::Send | wc -l` -eq 0"
+		}		
+		exec { 'Email-MIME':
+			command 	=> "cpanm Email::MIME",
+  			path 		=> $path,
+  			user 		=> root,
+ 			onlyif  	=> "test `puppet module list | grep Email::MIME | wc -l` -eq 0"
+		}	
+		exec { 'Template':
+			command 	=> "cpanm Template",
+  			path 		=> $path,
+  			user 		=> root,
+ 			onlyif  	=> "test `puppet module list | grep Template | wc -l` -eq 0"
+		}	
+		exec { 'URI':
+			command 	=> "cpanm URI",
+  			path 		=> $path,
+  			user 		=> root,
+ 			onlyif  	=> "test `puppet module list | grep URI | wc -l` -eq 0"
+		}		
+		exec { 'List-MoreUtils':
+			command 	=> "cpanm List::MoreUtils",
+  			path 		=> $path,
+  			user 		=> root,
+ 			onlyif  	=> "test `puppet module list | grep List::MoreUtils | wc -l` -eq 0"
+		}
+		exec { 'Math-Random-ISAAC':
+			command 	=> "cpanm Math::Random::ISAAC",
+  			path 		=> $path,
+  			user 		=> root,
+ 			onlyif  	=> "test `puppet module list | grep Math::Random::ISAAC | wc -l` -eq 0"
+		}
 	}
-	
+
 	#Declaration of the class
 	include perl
