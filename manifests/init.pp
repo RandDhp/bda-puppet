3755478df2d4fb46ea389b42e79ff6197225cdea
Diego De Nova <diego.dnova@gmail.com>
2014-03-13 17:12:28 +0000

    init.pp has changed

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,13 @@
-# This module triggers all the scripts needed for Bugzilla´s instalation.
+	#This module triggers all the scripts needed for Bugzilla´s instalation.
 
-include puppet-bugzilla-1.0.0::perl
-#include db
-#include web_server
-#include bugzilla
-#include ssl
+	#Definition of the class
+	class bda-puppet {
+		include perl
+		#include db
+		#include web_server
+		#include bugzilla
+		#include ssl
+	}
+
+	#Declaration of the class	
+	include bda-puppet
